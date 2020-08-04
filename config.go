d5620aa6100c11241076373ea577b77380098a80
depressi0n <793377875@qq.com>
2020-08-04 10:56:47 +0800

    modify name
    modify btcd to abec in the command arguments

--- config.go
+++ config.go
@@ -57,11 +57,11 @@ type config struct {
 	WalletPass string `long:"walletpass" default-mask:"-" description:"The public wallet password -- Only required if the wallet was created with one"`
 
 	// RPC client options
-	RPCConnect       string                  `short:"c" long:"rpcconnect" description:"Hostname/IP and port of btcd RPC server to connect to (default localhost:8334, testnet: localhost:18334, simnet: localhost:18556)"`
-	CAFile           *cfgutil.ExplicitString `long:"cafile" description:"File containing root certificates to authenticate a TLS connections with btcd"`
+	RPCConnect       string                  `short:"c" long:"rpcconnect" description:"Hostname/IP and port of abec RPC server to connect to (default localhost:8334, testnet: localhost:18334, simnet: localhost:18556)"`
+	CAFile           *cfgutil.ExplicitString `long:"cafile" description:"File containing root certificates to authenticate a TLS connections with abec"`
 	DisableClientTLS bool                    `long:"noclienttls" description:"Disable TLS for the RPC client -- NOTE: This is only allowed if the RPC client is connecting to localhost"`
-	AbecUsername     string                  `long:"btcdusername" description:"Username for btcd authentication"`
-	AbecPassword     string                  `long:"btcdpassword" default-mask:"-" description:"Password for btcd authentication"`
+	AbecUsername     string                  `long:"abecusername" description:"Username for abec authentication"`
+	AbecPassword     string                  `long:"abecpassword" default-mask:"-" description:"Password for abec authentication"`
 	Proxy            string                  `long:"proxy" description:"Connect via SOCKS5 proxy (eg. 127.0.0.1:9050)"`
 	ProxyUser        string                  `long:"proxyuser" description:"Username for proxy server"`
 	ProxyPass        string                  `long:"proxypass" default-mask:"-" description:"Password for proxy server"`
@@ -89,8 +89,8 @@ type config struct {
 	LegacyRPCListeners     []string                `long:"rpclisten" description:"Listen for legacy RPC connections on this interface/port (default port: 8332, testnet: 18332, simnet: 18554)"`
 	LegacyRPCMaxClients    int64                   `long:"rpcmaxclients" description:"Max number of legacy RPC clients for standard connections"`
 	LegacyRPCMaxWebsockets int64                   `long:"rpcmaxwebsockets" description:"Max number of legacy RPC websocket connections"`
-	Username               string                  `short:"u" long:"username" description:"Username for legacy RPC and btcd authentication (if btcdusername is unset)"`
-	Password               string                  `short:"P" long:"password" default-mask:"-" description:"Password for legacy RPC and btcd authentication (if btcdpassword is unset)"`
+	Username               string                  `short:"u" long:"username" description:"Username for legacy RPC and abec authentication (if abec is unset)"`
+	Password               string                  `short:"P" long:"password" default-mask:"-" description:"Password for legacy RPC and abec authentication (if abecpassword is unset)"`
 
 	// EXPERIMENTAL RPC server options
 	//
@@ -448,7 +448,9 @@ func loadConfig() (*config, []string, error) {
 
 		if !tempWalletExists {
 			// Perform the initial wallet creation wizard.
-			if err := createSimulationWallet(&cfg); err != nil {
+			//if err := createSimulationWallet(&cfg); err != nil {
+			//TODO(abe):
+			if err := createSimulationWalletAbe(&cfg); err != nil {
 				fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
 				return nil, nil, err
 			}
@@ -470,7 +472,12 @@ func loadConfig() (*config, []string, error) {
 		}
 
 		// Perform the initial wallet creation wizard.
-		if err := createWallet(&cfg); err != nil {
+		//if err := createWallet(&cfg); err != nil {
+		//	fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
+		//	return nil, nil, err
+		//}
+		//TODO(abe):
+		if err := createWalletAbe(&cfg); err != nil {
 			fmt.Fprintln(os.Stderr, "Unable to create wallet:", err)
 			return nil, nil, err
 		}
@@ -478,7 +485,7 @@ func loadConfig() (*config, []string, error) {
 		// Created successfully, so exit now with success.
 		os.Exit(0)
 	} else if !dbFileExists && !cfg.NoInitialLoad {
-		//	todo(ABE): remove the legacy parts (keystore)
+		//	todo(ABE): remove the legacy parts (keystore)  please do not do it!!
 		keystorePath := filepath.Join(netDir, keystore.Filename)
 		keystoreExists, err := cfgutil.FileExists(keystorePath)
 		if err != nil {
@@ -552,17 +559,17 @@ func loadConfig() (*config, []string, error) {
 				}
 				if !certExists {
 					if _, ok := localhostListeners[RPCHost]; ok {
-						btcdCertExists, err := cfgutil.FileExists(
+						abecCertExists, err := cfgutil.FileExists(
 							abecDefaultCAFile)
 						if err != nil {
 							fmt.Fprintln(os.Stderr, err)
 							return nil, nil, err
 						}
-						if btcdCertExists {
+						if abecCertExists {
 							cfg.CAFile.Value = abecDefaultCAFile
 						}
 					}
-				}
+				}                                   	
 			}
 		}
 	}
