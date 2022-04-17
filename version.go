53bca3cc29c2eb218cfbc96adf54a42f068ff1ba
chenshenwei1998 <393737680@qq.com>
2022-04-17 12:50:04 +0800

    Update version.go

--- version.go
+++ version.go
@@ -18,7 +18,7 @@ const (
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
-	appPreRelease = "alpha"
+	appPreRelease = "pre-alpha"
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
