9474276323b34c78045950c5ac6d088d9df2f63d
chenshenwei1998 <393737680@qq.com>
2023-01-08 19:12:08 +0800

    Update version.go

--- version.go
+++ version.go
@@ -14,7 +14,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 11
-	appPatch uint = 6
+	appPatch uint = 7
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
