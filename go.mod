f9cbf24e2ee51cdafccae1a5e26f71b3895ede91
chenshenwei1998 <393737680@qq.com>
2023-06-01 00:09:20 +0800

    Update version and dependency

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/abesuite/abewallet
 go 1.14
 
 require (
-	github.com/abesuite/abec v0.11.9
+	github.com/abesuite/abec v0.11.10
 	github.com/abesuite/go-socks v0.0.0-20170105172521-4720035b7bfd
 	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/websocket v1.4.2
--- version.go
+++ version.go
@@ -14,7 +14,7 @@ const semanticAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqr
 const (
 	appMajor uint = 0
 	appMinor uint = 11
-	appPatch uint = 9
+	appPatch uint = 10
 
 	// appPreRelease MUST only contain characters from semanticAlphabet
 	// per the semantic versioning spec.
